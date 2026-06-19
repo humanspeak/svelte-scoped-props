3056b77d72451c82ce4c52420b12598210f800eb
Jason Kummerl <jason.h@kummerl.com>
2026-06-19 17:53:57 -0400

    ci: exclude docs from root vitest run

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: ['**/node_modules/**', '**/dist/**', '**/.svelte-kit/**', '**/e2e/**'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/.svelte-kit/**', '**/e2e/**', '**/docs/**'],
     environment: 'node'
   }
 });
